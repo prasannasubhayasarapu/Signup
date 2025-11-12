1b23e338a71d4ad1d066014a37b532dac6e26179
prasannasubhayasarapu <subhaprasanna143@gmail.com>
2025-11-12 13:29:52 +0530

    coded in branch

--- index.js
+++ index.js
@@ -6,3 +6,27 @@ function signup(username) {
         return "Signup Successful, Please Login";
     }
 }
+//function 2
+function login(userName, password) {
+  let users = ["alice", "bob", "charlie"]; // Same user list for demo
+  const correctPassword = "Emp@123";
+
+  if (!users.includes(userName)) {
+    return "User Not Found, Please Signup";
+  } else if (password !== correctPassword) {
+    return "Wrong Password....";
+  } else {
+    return "Login Successful...";
+  }
+}
+//function2
+function login(userName, password) {
+    let users = ["alice", "bob", "charlie"]; 
+   if (!users.includes(userName)) {
+    return "User Not Found, Please Signup";
+  } else if (password !== correctPassword) {
+    return "Wrong Password....";
+  } else {
+    return "Login Successful...";
+  }
+}
